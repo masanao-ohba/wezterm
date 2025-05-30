98fae27fb889047f6d1e815f4a0d9eda4d3ac6f9
Masanao Ohba <masanao.oba@dragon.jp>
2025-05-31 01:45:54 +0900

    fix: Swtiching Workspace FUZZY

--- keymap.lua
+++ keymap.lua
@@ -16,7 +16,31 @@ return {
       -- workspaceの切り替え
       key = "w",
       mods = "LEADER",
-      action = act.ShowLauncherArgs({ flags = "WORKSPACES", title = "Select workspace" }),
+      action = wezterm.action_callback (function (win, pane)
+        -- workspace のリストを作成
+        local workspaces = {}
+        for i, name in ipairs(wezterm.mux.get_workspace_names()) do
+          table.insert(workspaces, {
+            id = name,
+            label = string.format("%d. %s", i, name),
+          })
+        end
+        local current = wezterm.mux.get_active_workspace()
+        -- 選択メニューを起動
+        win:perform_action(act.InputSelector {
+          action = wezterm.action_callback(function (_, _, id, label)
+            if not id and not label then
+              wezterm.log_info "Workspace selection canceled"  -- 入力が空ならキャンセル
+            else
+              win:perform_action(act.SwitchToWorkspace { name = id }, pane)  -- workspace を移動
+            end
+          end),
+          title = "Select workspace",
+          choices = workspaces,
+          fuzzy = true,
+          fuzzy_description = string.format("Select workspace: %s -> ", current), -- requires nightly build
+        }, pane)
+      end),
     },
     {
       --workspaceの名前変更
